a37161575428fd1e8ff9120317b57f269e31d99f
sagunjaiswal <sagunjaiswal99@gmail.com>
2022-07-05 19:59:22 +0530

    completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { CliComponentComponent } from './cli-component/cli-component.component';
+import {JumbotronManualComponentComponent} from './jumbotron-manual-component/manual-component.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CliComponentComponent,
+    JumbotronManualComponentComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/jumbotron-manual-component/manual-component.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-jumbotron-manual-component',
+    templateUrl: './manual-component.component.html',
+    styleUrls : []
+})
+export class JumbotronManualComponentComponent{
+
+}
